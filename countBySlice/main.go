3d7a4d04d1c6ef34da7f4eb670116c11eefe4919
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-07 20:33:50 +0200

    using slices - but really should be maps

---
+++ countBySlice/main.go
@@ -0,0 +1,37 @@
+// count tallies the number of times each line occurs within a file.
+package main
+
+import (
+	"fmt"
+	"github.com/nadgerz/go/datafile"
+	"log"
+)
+
+func main() {
+	lines, err := datafile.GetStrings("votes.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var names []string
+	var counts []int
+
+	for _, line := range lines {
+		matched := false
+		for i, name := range names {
+			if name == line {
+				counts[i]++
+				matched = true
+			}
+		}
+
+		if matched == false {
+			names = append(names, line)
+			counts = append(counts, 1)
+		}
+	}
+
+	for i, name := range names {
+		fmt.Printf("%s: %d\n", name, counts[i])
+	}
+}
