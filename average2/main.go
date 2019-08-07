1bde7ed90dfe7e6c12ae0a015be2f54cdc06324c
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-07 17:15:47 +0200

    update

--- average2/main.go
+++ average2/main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
 	fmt.Println(os.Args)
 	fmt.Println(os.Args[1:])
+
+	arguments := os.Args[1:]
+
+	var sum float64 = 0
+
+	for _, argument := range arguments {
+
+		number, err := strconv.ParseFloat(argument, 64)
+		if err != nil {
+			log.Fatal(err)
+			// return nil, err
+		}
+
+		sum += number
+	}
+
+	sampleCount := float64(len(arguments))
+	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
 }
