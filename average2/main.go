a67ab088529571d02744ca327b6a1f36b964c580
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-07 17:36:59 +0200

    update

--- average2/main.go
+++ average2/main.go
@@ -7,6 +7,17 @@ import (
 	"strconv"
 )
 
+func average(numbers ...float64) float64 {
+	var sum float64 = 0
+
+	// process each variadic argument
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum / float64(len(numbers))
+}
+
 func main() {
 	// fmt.Println(os.Args)
 	// fmt.Println(os.Args[1:])
@@ -28,4 +39,7 @@ func main() {
 
 	sampleCount := float64(len(arguments))
 	fmt.Printf("Average: %0.2f\n", sum/sampleCount)
+
+	fmt.Println(average(100, 50))
+	fmt.Println(average(90.7, 89.7, 98.5, 92.3))
 }
