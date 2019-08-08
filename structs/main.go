899a9cd61baeb35797124130c49c31390df734a4
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-08 15:16:00 +0200

    update

--- structs/main.go
+++ structs/main.go
@@ -4,20 +4,36 @@ import (
 	"fmt"
 )
 
-var subscriber struct {
+var subscriber1 struct {
+	name   string
+	rate   float64
+	active bool
+}
+
+var subscriber2 struct {
 	name   string
 	rate   float64
 	active bool
 }
 
 func main() {
-	subscriber.name = "Steve Ingram"
-	subscriber.rate = 4.99
-	subscriber.active = true
+	subscriber1.name = "Steve Ingram"
+	subscriber1.rate = 4.99
+	subscriber1.active = true
 
-	fmt.Println("Name:", subscriber.name)
-	fmt.Println("Monthly Rate:", subscriber.rate)
-	fmt.Println("Active?:", subscriber.active)
+	fmt.Println("Name:", subscriber1.name)
+	fmt.Println("Monthly Rate:", subscriber1.rate)
+	fmt.Println("Active?:", subscriber1.active)
 
-	fmt.Printf("%#v\n", subscriber)
+	fmt.Printf("%#v\n", subscriber1)
+
+	subscriber2.name = "Kerstin Dengl"
+	subscriber2.rate = 5.99
+	subscriber2.active = false
+
+	fmt.Println("Name:", subscriber2.name)
+	fmt.Println("Monthly Rate:", subscriber2.rate)
+	fmt.Println("Active?:", subscriber2.active)
+
+	fmt.Printf("%#v\n", subscriber2)
 }
