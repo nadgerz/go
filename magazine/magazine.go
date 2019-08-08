7a7ae1969dba029e992cdb8b709321cfc2f8e1ac
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-08 16:35:38 +0200

    HomeAddress

--- magazine/magazine.go
+++ magazine/magazine.go
@@ -1,12 +1,21 @@
 package magazine
 
 type Subscriber struct {
-	Name   string
-	Rate   float64
-	Active bool
+	Name        string
+	Rate        float64
+	Active      bool
+	HomeAddress Address
 }
 
 type Employee struct {
-	Name   string
-	Salary float64
+	Name        string
+	Salary      float64
+	HomeAddress Address
+}
+
+type Address struct {
+	Street     string
+	City       string
+	State      string
+	PostalCode string
 }
