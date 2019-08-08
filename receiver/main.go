417e8151cb91510a32550def42d43f95b73d8229
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-08 18:55:10 +0200

    update

---
+++ receiver/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type MyType string
+
+func (m MyType) sayHi() {
+	fmt.Println("Hi from", m)
+}
+
+func main() {
+	fmt.Println(os.Args)
+
+	value := MyType("a MyType value")
+	value.sayHi()
+
+	anotherValue := MyType("another value")
+	anotherValue.sayHi()
+
+	x := 3.14
+	fmt.Println(x)
+
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%T\n", value)
+}
