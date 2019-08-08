2d03a427d16fe210c3bcfacedb9d90b595063569
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-08 14:08:37 +0200

    update

--- maps/main.go
+++ maps/main.go
@@ -10,6 +10,13 @@ func main() {
 	map1 = make(map[string]int)
 	fmt.Printf("%#v\n", map1)
 
+	map1["gold"] = 1
+	map1["silver"] = 2
+	map1["bronze"] = 3
+
+	fmt.Println(map1["bronze"])
+	fmt.Println(map1["gold"])
+
 	map2 := make(map[string]int)
 	fmt.Printf("%#v\n", map2)
 
