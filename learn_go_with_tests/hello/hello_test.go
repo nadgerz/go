cf84dc236a523df705d7fabff7e959a2b810862f
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-18 15:32:51 +0200

    pass in language - default to English

--- learn_go_with_tests/hello/hello_test.go
+++ learn_go_with_tests/hello/hello_test.go
@@ -14,7 +14,7 @@ func TestHello(t *testing.T) {
 
 	t.Run("saying hello to people", func(t *testing.T) {
 
-		got := Hello("Steve")
+		got := Hello("Steve", "")
 		want := "Hello, Steve"
 
 		assertCorrectMessage(t, got, want)
@@ -22,8 +22,16 @@ func TestHello(t *testing.T) {
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 
 		assertCorrectMessage(t, got, want)
 	})
