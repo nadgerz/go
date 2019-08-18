d5eb85128f064031aff59654949f669075ee8ccf
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-18 15:34:17 +0200

    constants could be better

--- learn_go_with_tests/hello/hello.go
+++ learn_go_with_tests/hello/hello.go
@@ -2,11 +2,17 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
