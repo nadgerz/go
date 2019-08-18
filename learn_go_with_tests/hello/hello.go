af4fcafff8a8197c6b2bd5509039c3df614497e5
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-18 15:53:47 +0200

    switch to switch

--- learn_go_with_tests/hello/hello.go
+++ learn_go_with_tests/hello/hello.go
@@ -14,15 +14,16 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
+	prefix := englishHelloPrefix
+
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
 	}
 
-	if language == french {
-		return frenchHelloPrefix + name
-	}
-
-	return englishHelloPrefix + name
+	return prefix + name
 }
 
 func main() {
