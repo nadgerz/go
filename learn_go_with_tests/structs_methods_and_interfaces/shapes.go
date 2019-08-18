277ee53ca6aa312f50775f390624b5a566143550
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-18 23:57:20 +0200

    trying to test different shapes

--- learn_go_with_tests/structs_methods_and_interfaces/shapes.go
+++ learn_go_with_tests/structs_methods_and_interfaces/shapes.go
@@ -1,5 +1,14 @@
 package smi
 
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
 func Perimeter(rect Rectangle) float64 {
 	return 2 * (rect.Width + rect.Height)
 }
--- learn_go_with_tests/structs_methods_and_interfaces/shapes_test.go
+++ learn_go_with_tests/structs_methods_and_interfaces/shapes_test.go
@@ -14,12 +14,38 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
 
-	got := Area(rectangle)
-	want := 100.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
 
-	if got != want {
-		t.Errorf("got %0.2f want %0.2f", got, want)
-	}
+		got := Area(rectangle)
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("bad rectangle - negative widths", func(t *testing.T) {
+		rectangle := Rectangle{-1, 6}
+
+		got := Area(rectangle)
+		// Would rather pass back an error 'object'
+		want := -1.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+
+		got := Area(circle)
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 }
