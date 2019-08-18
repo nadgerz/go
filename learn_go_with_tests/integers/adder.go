1740f92b0ead4b490c6f28c8ae179c9ead6e0039
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-18 19:57:33 +0200

    Simplest test to pass

---
+++ learn_go_with_tests/integers/adder.go
@@ -0,0 +1,9 @@
+// package integers has functions to manipulate integers
+package integers
+
+import ()
+
+// Add takes two integers and returns the sum of them
+func Add(x, y int) int {
+	return x + y
+}
