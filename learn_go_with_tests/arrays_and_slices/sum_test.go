3e386bf2cdc734359ef49211d3cd5afaf48f5962
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-18 21:01:01 +0200

    duplicate

--- learn_go_with_tests/arrays_and_slices/sum_test.go
+++ learn_go_with_tests/arrays_and_slices/sum_test.go
@@ -6,18 +6,6 @@ import (
 
 func TestSum(t *testing.T) {
 
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-
-		numbers := []int{1, 2, 3, 4, 5}
-
-		got := Sum(numbers)
-		want := 15
-
-		if want != got {
-			t.Errorf("got '%d' want '%d' given, %v", got, want, numbers)
-		}
-	})
-
 	t.Run("collection of any numbers", func(t *testing.T) {
 
 		numbers := []int{1, 2, 3}
