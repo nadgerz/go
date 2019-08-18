c006e6b202ded74811324e545b6d50aa58f89f03
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-18 20:28:28 +0200

    imports removed

--- learn_go_with_tests/integers/adder.go
+++ learn_go_with_tests/integers/adder.go
@@ -1,8 +1,6 @@
 // package integers has functions to manipulate integers
 package integers
 
-import ()
-
 // Add takes two integers and returns the sum of them
 func Add(x, y int) int {
 	return x + y
