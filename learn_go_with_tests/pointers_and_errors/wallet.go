12d1e57695f435b59660698ad34620057ef4d41e
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-19 16:26:15 +0200

    learn how to create a struct

--- learn_go_with_tests/pointers_and_errors/wallet.go
+++ learn_go_with_tests/pointers_and_errors/wallet.go
@@ -1,5 +1,12 @@
 package pae
 
 type Wallet struct {
-	Balance float64
+	balance float64
+}
+
+func (w Wallet) Deposit(amount float64) {
+}
+
+func (w Wallet) Balance() float64 {
+	return 0
 }
--- learn_go_with_tests/pointers_and_errors/wallet_test.go
+++ learn_go_with_tests/pointers_and_errors/wallet_test.go
@@ -7,12 +7,12 @@ import (
 func TestWallet(t *testing.T) {
 
 	t.Run("deposit", func(t *testing.T) {
-		wallet := Wallet()
+		wallet := Wallet{}
 
-		wallet.Deposit(10)
+		wallet.Deposit(10.0)
 
 		got := wallet.Balance()
-		want := 10
+		want := 10.0
 
 		if got != want {
 			t.Errorf("%#v got %.2f want %.2f", want, got, want)
