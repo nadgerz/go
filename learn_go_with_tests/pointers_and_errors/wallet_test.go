82c662b5deaeedbf59b050962c9a40dca4f41207
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-19 16:21:05 +0200

    add in test

--- learn_go_with_tests/pointers_and_errors/wallet_test.go
+++ learn_go_with_tests/pointers_and_errors/wallet_test.go
@@ -8,5 +8,15 @@ func TestWallet(t *testing.T) {
 
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet()
+
+		wallet.Deposit(10)
+
+		got := wallet.Balance()
+		want := 10
+
+		if got != want {
+			t.Errorf("%#v got %.2f want %.2f", want, got, want)
+		}
+
 	})
 }
