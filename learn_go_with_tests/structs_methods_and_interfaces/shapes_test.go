77aa640e3969134bc05b2479cfb7818044f4c9ef
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-19 14:26:06 +0200

    naming a problem?

--- learn_go_with_tests/structs_methods_and_interfaces/shapes_test.go
+++ learn_go_with_tests/structs_methods_and_interfaces/shapes_test.go
@@ -18,6 +18,49 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
+func TestAreas(t *testing.T) {
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
+	}
+}
+
+func TestAreaFunctions(t *testing.T) {
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{
+			Rectangle{12, 6},
+			72.0,
+		},
+		{
+			Circle{10},
+			314.1592653589793,
+		},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
+	}
+}
+
 func TestArea(t *testing.T) {
 
 	checkArea := func(t *testing.T, shape Shape, want float64) {
