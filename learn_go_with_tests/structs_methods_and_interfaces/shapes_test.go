33896de3bffca2973180c6be8487ba59f273d82a
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-19 14:27:30 +0200

    So it WAS naming.. weird!

--- learn_go_with_tests/structs_methods_and_interfaces/shapes_test.go
+++ learn_go_with_tests/structs_methods_and_interfaces/shapes_test.go
@@ -24,25 +24,6 @@ func TestAreas(t *testing.T) {
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
-	}
-
-	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %.2f want %.2f", got, tt.want)
-		}
-	}
-}
-
-func TestAreaFunctions(t *testing.T) {
-
-	areaTests := []struct {
-		shape Shape
-		want  float64
-	}{
 		{
 			Rectangle{12, 6},
 			72.0,
@@ -54,7 +35,8 @@ func TestAreaFunctions(t *testing.T) {
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
+		got, _ := tt.shape.Area()
+
 		if got != tt.want {
 			t.Errorf("got %.2f want %.2f", got, tt.want)
 		}
