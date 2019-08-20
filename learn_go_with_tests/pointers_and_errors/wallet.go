4214688a192806b3cb01e35e0fe32932a99881f5
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-20 15:29:34 +0200

    oops, forgot this exported error object/value

--- learn_go_with_tests/pointers_and_errors/wallet.go
+++ learn_go_with_tests/pointers_and_errors/wallet.go
@@ -15,6 +15,8 @@ type Wallet struct {
 	balance Bitcoin
 }
 
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
 func (w *Wallet) Deposit(amount Bitcoin) {
 	// fmt.Printf("address of balance in Deposit is %v\n", &w.balance)
 	w.balance += amount
@@ -22,7 +24,7 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 
 func (w *Wallet) Withdraw(amount Bitcoin) error {
 	if amount > w.balance {
-		return errors.New("cannot withdraw, insufficient funds")
+		return ErrInsufficientFunds
 	}
 
 	w.balance -= amount
