6009fb36344b07f317d3294de8b75123f82d1e5a
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-20 23:20:57 +0200

    Need some more practice here...
    But this is pulling out the std code.

--- learn_go_with_tests/maps/dictionary_test.go
+++ learn_go_with_tests/maps/dictionary_test.go
@@ -30,16 +30,25 @@ func TestSearch(t *testing.T) {
 
 func TestAdd(t *testing.T) {
 	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
 
-	want := "this is just a test"
-	got, err := dictionary.Search("test")
+	word := "test"
+	definition := "this is just a test"
+
+	dictionary.Add(word, definition)
+
+	assertDefinition(t, dictionary, word, definition)
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
 	if err != nil {
 		t.Fatal("should find added word:", err)
 	}
 
-	if want != got {
-		t.Errorf("got %q want %q", got, want)
+	if definition != got {
+		t.Errorf("got %q want %q", got, definition)
 	}
 }
 
