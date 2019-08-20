771c1095d319e3bdf8ba776912bd4876dc76b89b
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-20 14:27:35 +0200

    satisfy the compiler - for now return nil

--- learn_go_with_tests/pointers_and_errors/wallet.go
+++ learn_go_with_tests/pointers_and_errors/wallet.go
@@ -17,8 +17,9 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
 	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
