0faa044f15e71039c990f66f710555c11cbe7567
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-22 17:57:30 +0200

    Starting to use Spies

--- learn_go_with_tests/mocking/countdown.go
+++ learn_go_with_tests/mocking/countdown.go
@@ -6,6 +6,18 @@ import (
 	"time"
 )
 
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func Countdown(out io.Writer) {
 	for i := 3; i > 0; i-- {
 		time.Sleep(1 * time.Second)
