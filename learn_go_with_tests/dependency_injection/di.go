6315795c0662cc8aed915c40bb557e8439a753ca
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-22 17:34:02 +0200

    ./di.go:14:7: cannot use os.Stdout (type *os.File) as type *bytes.Buffer in argument to Greet

--- learn_go_with_tests/dependency_injection/di.go
+++ learn_go_with_tests/dependency_injection/di.go
@@ -1,10 +1,15 @@
-package di
+package main
 
 import (
 	"bytes"
 	"fmt"
+	"os"
 )
 
 func Greet(writer *bytes.Buffer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Kerstin")
 }
