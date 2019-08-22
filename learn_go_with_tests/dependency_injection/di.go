16f71b0a0f704084b72aca1526e8703cbe0422bb
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-22 17:37:43 +0200

    What other places can we write data to using io.Writer? Just how general purpose is our Greet function?
    When you write an HTTP handler, you are given an http.ResponseWriter and the http.Request that was used to make the request. When you implement your server you write your response using the writer.
    
    You can probably guess that http.ResponseWriter also implements io.Writer so this is why we could re-use our Greet function inside our handler.

--- learn_go_with_tests/dependency_injection/di.go
+++ learn_go_with_tests/dependency_injection/di.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
 }
 
 func main() {
 	Greet(os.Stdout, "Kerstin")
+
+	http.ListenAndServe(":5555", http.HandlerFunc(MyGreeterHandler))
 }
