7b5eee6a2db3acec44a838207749c9516236c732
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-22 17:54:26 +0200

    Slooooooow

--- learn_go_with_tests/mocking/countdown.go
+++ learn_go_with_tests/mocking/countdown.go
@@ -3,12 +3,15 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 func Countdown(out io.Writer) {
 	for i := 3; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
 
+	time.Sleep(1 * time.Second)
 	fmt.Fprint(out, "Go!")
 }
