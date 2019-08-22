165946030ce3fcf5bacfc5608394e8e35e121df4
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-22 17:29:06 +0200

    ./di_test.go:10:7: too many arguments in call to Greet 	have (*bytes.Buffer, string) 	want (string)

--- learn_go_with_tests/dependency_injection/di_test.go
+++ learn_go_with_tests/dependency_injection/di_test.go
@@ -1,8 +1,18 @@
 package di
 
 import (
+	"bytes"
 	"testing"
 )
 
 func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Chris")
+
+	got := buffer.String()
+	want := "hello, Chris"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
