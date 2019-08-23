bbe0c5b91742fabbe5320832ec64cc72ae6bffcd
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-08-23 18:41:09 +0200

    Gotta love function assignment.

--- learn_go_with_tests/concurrency/concurrency.go
+++ learn_go_with_tests/concurrency/concurrency.go
@@ -3,8 +3,14 @@ package concurrency
 import "time"
 
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
-func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+var CheckWebsites = CheckWebsites_004
+
+func CheckWebsites_000(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 
 	for _, url := range urls {
@@ -50,6 +56,24 @@ func CheckWebsites_003(wc WebsiteChecker, urls []string) map[string]bool {
 	}
 
 	time.Sleep(2 * time.Second)
+
+	return results
+}
+
+func CheckWebsites_004(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+
+	for _, url := range urls {
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
 
 	return results
 }
