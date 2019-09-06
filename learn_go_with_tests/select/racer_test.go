f04a2d3ffe9194147b969057dc572f17e0220aa1
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-09-06 18:07:28 +0200

    fake out stuff with httptest

--- learn_go_with_tests/select/racer_test.go
+++ learn_go_with_tests/select/racer_test.go
@@ -1,10 +1,24 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.co.uk"
+	slowServer := httptest.NewServer(http.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatsusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatsusOK)
+	}))
+
+	slowURL := slowServer
+	fastURL := fastServer
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
@@ -12,4 +26,7 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
