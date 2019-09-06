cf3ccfe5a94b5f5d017913b4d565eaae65a360d9
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-09-06 18:45:13 +0200

    All well and good but this now takes 10 seconds to run

--- learn_go_with_tests/select2/racer.go
+++ learn_go_with_tests/select2/racer.go
@@ -1,7 +1,9 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
 /*
@@ -25,6 +27,8 @@ func Racer(a, b string) (winner string, error error) {
 		return a, nil
 	case <-ping(b):
 		return b, nil
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
 
