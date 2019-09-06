75463e10c36d42f404c4de0c5ff5496172af6b02
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-09-06 18:31:48 +0200

    Using channels and goroutines.

---
+++ learn_go_with_tests/select2/racer.go
@@ -0,0 +1,49 @@
+package racer
+
+import (
+	"net/http"
+)
+
+/*
+If you recall from the concurrency chapter, you can wait for
+values to be sent to a channel with myVar := <-ch.
+
+This is a blocking call, as you're waiting for a value.
+
+What select lets you do is wait on multiple channels.
+
+The first one to send a value "wins" and the code underneath the case is executed.
+
+We use ping in our select to set up two channels for each of our URLs.
+
+Whichever one writes to its channel first will have its code executed in the select,
+which results in its URL being returned (and being the winner).
+*/
+func Racer(a, b string) (winner string) {
+	select {
+	case <-ping(a):
+		return a
+	case <-ping(b):
+		return b
+	}
+}
+
+/*
+We have defined a function ping which creates a chan bool and returns it.
+
+In our case, we don't really care what the type sent in the channel,
+we just want to send a signal to say we're finished so booleans are fine.
+
+Inside the same function, we start a goroutine which will send a signal
+into that channel once we have completed http.Get(url).
+*/
+func ping(url string) chan bool {
+	ch := make(chan bool)
+
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+
+	return ch
+}
